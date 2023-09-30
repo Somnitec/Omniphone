ad631d8aed04311ab8679a07e1ceda0325ee53fc
somnitec <arvidj@gmail.com>
2023-09-30 18:12:34 +0200

    LCD and touch functional

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,28 @@
 #include <Arduino.h>
-#include <Wire.h>
 #include "Adafruit_MPR121.h"
+#include "1.28inch_Touch_LCD/LCD_Driver.h"
+#include "1.28inch_Touch_LCD/Touch_Driver.h"
+#include "1.28inch_Touch_LCD/GUI_Paint.h"
+
+
 
 Adafruit_MPR121 cap = Adafruit_MPR121();
 
 uint16_t lasttouched = 0;
 uint16_t currtouched = 0;
+UWORD x, y = 0;
+
+void Touch_INT_callback() {
+  XY = Touch_1IN28_Get_Point();
+
+        x = XY.x_point;
+        y = XY.y_point;
+        Serial.print("touched at: ");
+        Serial.print(x);
+        Serial.print(',');
+        Serial.print(y);
+        Serial.println();
+}
 
 void setup() {
   Serial.begin(9600);
@@ -15,6 +32,25 @@ void setup() {
     while (1);
   }
   Serial.println("MPR121@0x5B found!");
+
+  Touch_1IN28_XY XY;
+  XY.mode = 1;
+  Config_Init();
+  LCD_Init();
+  if (Touch_1IN28_init(XY.mode) == true)
+    Serial.println("Touchscreen OK!");
+  else
+    Serial.println("Touchscreen NO!");
+
+  LCD_SetBacklight(255);//0-255
+  Paint_NewImage(LCD_WIDTH, LCD_HEIGHT, 0, BLACK);
+  Paint_Clear(WHITE);
+  attachInterrupt(digitalPinToInterrupt(TP_INT_PIN), Touch_INT_callback, LOW);
+  pinMode(TP_INT_PIN, INPUT_PULLUP);
+  Paint_DrawString_EN(35, 90, "OMNIPHONE!", &Font20, BLACK, WHITE);
+
+
+
 }
 
 void loop() {
@@ -33,5 +69,8 @@ void loop() {
   }
   lasttouched = currtouched;
 
+    
+
   delay(100);           
 }
+
